74f9206e28b0b52ff96ab292edb6f7c161fb2e7b
Dagttatray Kale <way2datta@gmail.com>
2019-02-08 22:53:49 +0530

    Divisible by 5 test is passing.

--- csharp/BeingCraftsman.FizzBuzzGenerator/BeingCraftsman.FizzBuzz/FizzBuzzGenerator.cs
+++ csharp/BeingCraftsman.FizzBuzzGenerator/BeingCraftsman.FizzBuzz/FizzBuzzGenerator.cs
@@ -6,10 +6,7 @@ namespace BeingCraftsman.FizzBuzz
     {
         public static string Generate(int input)
         {
-            if (input == 10)
-            {
-                return "Buzz";
-            }
+
 
             if (input == 15)
             {
@@ -19,6 +16,11 @@ namespace BeingCraftsman.FizzBuzz
             if(input == 17)
             {
                 return "17"; 
+            }
+
+            if (input % 5 == 0)
+            {
+                return "Buzz";
             }
 
             if (input % 3 == 0)
