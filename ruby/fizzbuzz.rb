ea5631f8a793d985de617f79f355c827ac290b1f
Sushant Gawali <sushant@technogise.com>
2019-02-15 23:59:05 +0530

    (ruby) GREEN: added code and passing tests

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -1,4 +1,26 @@
 class Fizzbuzz
     def self.generate(num)
+        if self.is_divisible_by_3(num) and self.is_divisible_by_5(num)
+            return "fizzbuzz"
+        end
+
+        if self.is_divisible_by_3(num)
+            return "fizz"
+        end
+
+        if self.is_divisible_by_5(num)
+            return "buzz"
+        end
+
+        num.to_s
+    end
+
+    private
+    def self.is_divisible_by_3(num)
+        num % 3 == 0
+    end
+
+    def self.is_divisible_by_5(num)
+        num % 5 == 0
     end
 end
