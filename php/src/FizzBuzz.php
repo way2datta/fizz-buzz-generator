0e38d59170b47fe6b91c472c28093494ccfd517f
Sushant Gawali <sushant@technogise.com>
2019-02-16 09:29:51 +0530

    (PHP) GREEN: added code and tests are passing

--- php/src/FizzBuzz.php
+++ php/src/FizzBuzz.php
@@ -17,5 +17,36 @@ class FizzBuzz {
     public static function generate(int $number) : string
     {
 
+        if (self::isDivisibleBy3($number) && self::isDivisibleBy5($number)) return "FizzBuzz";
+
+        if (self::isDivisibleBy3($number)) return "Fizz";
+
+        if (self::isDivisibleBy5($number)) return "Buzz";
+
+        return $number;
+    }
+
+    /**
+     * Check if number is divisible by 3
+     *
+     * @param int $number number to check
+     *
+     * @return bool
+     */
+    private static function isDivisibleBy3(int $number) : bool
+    {
+        return $number % 3 === 0;
+    }
+
+    /**
+     * Check if number is divisible by 5
+     *
+     * @param int $number number to check
+     *
+     * @return bool
+     */
+    private static function isDivisibleBy5(int $number) : bool
+    {
+        return $number % 5 === 0;
     }
 }
